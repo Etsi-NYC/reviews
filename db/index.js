2b42e98ea5f9bb39e9079d94188bbc46366d9631
Wyatt <mrwnt10@gmail.com>
2018-10-24 12:55:07 -0400

    Refactor db/index.js to not drop tables and only add tables if they do not exist

--- db/index.js
+++ db/index.js
@@ -8,9 +8,10 @@ var knex = require('knex')({
   }
 });
 
-knex.schema.dropTableIfExists('items')
-  .then(() => {
-    console.log('items table dropped')
+knex.schema.hasTable('items').then((exists) => {
+  console.log(exists);
+  if (!exists) {
+    console.log('items table doesn\'t exist')
     knex.schema.createTable('items', (table) => {
       table.string('name');
       table.integer('id').primary();
@@ -21,25 +22,28 @@ knex.schema.dropTableIfExists('items')
         knex('items').insert({name:'Birdhouse', id:1, image_path: 'www.google.com/birdhouseimage'})
           .then(() => {
             console.log('item inserted')
-            knex.schema.dropTableIfExists('reviews')
-            .then(() => {
-              knex.schema.createTable('reviews', (table) => {
-                table.string('username');
-                table.date('date');
-                table.decimal('rating');
-                table.string('comment');
-                table.integer('item_id');
-                table.foreign('item_id').references('id').inTable('items');
-              })
-                .then((table) => {
-                  console.log('reviews table created');
-                  knex('reviews').insert({username: 'Josh', date: '1991-01-01', rating: 4.5, comment: 'this is a comment', item_id: 1})
-                    .then((insertId) => console.log(`review inserted at row ${insertId}`))
+            knex.schema.hasTable('reviews').then((exists) => {
+              if (!exists) {
+                knex.schema.createTable('reviews', (table) => {
+                  table.string('username');
+                  table.date('date');
+                  table.decimal('rating');
+                  table.integer('item_id');
+                  table.foreign('item_id').references('id').inTable('items');
+                  table.string('comment', 2000);
                 })
+                  .then((table) => {
+                    console.log('reviews table created');
+                    knex('reviews').insert({username: 'Josh', date: '1991-01-01', rating: 4.5, comment: 'this is a comment', item_id: 1})
+                      .then((insertId) => console.log(`review inserted at row ${insertId}`))
+                  })
+              }
             })
-          });
+          })
       })
-  })
+  }
+});
+
 
 
 
