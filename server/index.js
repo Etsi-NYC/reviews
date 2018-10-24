10850d5f327d3f2b71c92ee681c1c2e26bd26b04
Wyatt <mrwnt10@gmail.com>
2018-10-24 19:53:24 -0400

    Handle GET /reviews requests, including cases where only the item name or neither the item name nor id is received.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path')
+const path = require('path');
+const models = require('./../db/models');
 
 const app = express();
 var port = process.env.PORT || 3456;
@@ -10,5 +11,20 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './../client/dist')));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.get('/reviews', (req, res) => {
+  console.log(req.query.id);
+  if (!req.query.id && !req.query.name) res.status(400).send('Must include either item ID or item name in query')
+  if (!req.query.id) {
+    models.itemNameToId(req.query.name)
+      .then((id) => {
+        return models.fetchReviewsById(id)
+      })
+      .then((reviews) => res.send(reviews));
+  } else {
+    models.fetchReviewsById(req.query.id)
+      .then((reviews) => res.send(reviews));
+  }
+})
 
 app.listen(port, () => console.log(`Server now listening on port ${port}`));
