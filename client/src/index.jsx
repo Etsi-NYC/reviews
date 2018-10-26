b5d268677973440a7cafe32077b1c8a966216863
Wyatt <mrwnt10@gmail.com>
2018-10-26 11:40:03 -0400

    Finish hardcoded front-end

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -53,7 +53,8 @@ const ReviewsList = styled.ul`
 `
 
 const FadeGradient = styled.div`
-position:absolute;
+  display: ${props => props.visible ? 'block' : 'none'};
+  position:absolute;
   z-index:2;
   right:0; bottom:0; left:0;
   height:6rem;
@@ -67,18 +68,45 @@ position:absolute;
 `
 
 const MoreLink = styled.a`
+  display: ${props => props.visible ? 'block' : 'none'}; 
   font-size: 1rem;
   font-weight: bold;
   text-decoration: underline;
   color: black;
+  cursor: pointer;
   :hover {
     color: #595959;
   }
 `
 
+const AllReviewsButton = styled.button`
+  background: black;
+  color: white;
+  display: ${props => props.visible ? 'block' : 'none'}; 
+  padding: 0.57rem 0.86rem;
+  font-size: 1rem;
+  font-weight: bold;
+  border-radius: 0.21rem; 
+  cursor: pointer;
+  border: black;
+`
 
+const PhotosFromReviewsTitle = styled.div`
+  font-size: 1rem;
+  padding: 1.28rem 0 0.86rem 0;
+  font-weight: 500;
+`
+
+const PhotofromReviews = styled.img`
+  padding: 0rem 1.29rem 0.64rem 0;
+  float: left;
+  width: 7.11rem;
+  height: auto;
+`
 
 const nums = [1,2,3,4,5,6,7,8];
+
+const photos = [1,2,3,4];
 
 class Reviews extends React.Component {
   constructor(props) {
@@ -103,9 +131,12 @@ class Reviews extends React.Component {
           </Header>
           <ReviewsList showMore={this.state.showMore}>
             {nums.map((num) => <ReviewDiv><Review/></ReviewDiv>)}
-            <FadeGradient/> 
+            <FadeGradient visible={!this.state.showMore}/> 
           </ReviewsList>
-          <MoreLink onClick={this.handleShowMoreClick}>+ More</MoreLink>
+          <MoreLink onClick={this.handleShowMoreClick} visible={!this.state.showMore}>+ More</MoreLink>
+          <AllReviewsButton visible={this.state.showMore} href='/'>Read All Reviews (202)</AllReviewsButton>
+          <PhotosFromReviewsTitle>Photos from reviews</PhotosFromReviewsTitle>
+          {photos.map((photo) => <PhotofromReviews src="https://is1-ssl.mzstatic.com/image/thumb/Purple71/v4/47/cf/cf/47cfcf79-9e1d-b21f-8e10-2658b7650c15/mzl.oiljceng.png/246x0w.jpg"/>)}
         </StyledDiv>
       </React.Fragment>
     )
